52018560824fe47a980ceeb472420667428de481
JulSeb42 <julien.sebag@me.com>
2023-08-09 18:49:36 +0100

    Test fix deploy

---
+++ client/src/components/Tabs/styles.tsx
@@ -0,0 +1,7 @@
+/*=============================================== Tabs styles ===============================================*/
+
+import styled from "styled-components"
+
+export const StyledTabs = styled.div`
+	
+`
---
+++ client/src/components/Tabs/types.ts
@@ -0,0 +1,7 @@
+/*=============================================== Tabs types ===============================================*/
+
+import type { ElementType } from "react"
+
+export interface TabsProps {
+	as?: ElementType
+}
